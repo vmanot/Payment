7e1624ccb4dba72fe4906fbfba3fec5a91298eb6
Vatsal Manot <vatsal.manot@yahoo.com>
2020-08-07 18:07:01 +0530

    Update package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -15,8 +15,5 @@ let package = Package(
     ],
     targets: [
         .target(name: "Payment", dependencies: [], path: "Sources"),
-    ],
-    swiftLanguageVersions: [
-        .version("5.1")
     ]
 )
